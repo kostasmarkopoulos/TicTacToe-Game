5761698ca2d5f0b687073945057cb513f2e5ffb9
Kostas Markopoulos <kostasmark151@gmail.com>
2022-03-29 21:09:21 +0300

    initial Commit : JavaFX

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -17,9 +19,18 @@ public class Main extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception{
-        //Stage stage = new Stage();
-        Group root = new Group();
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Group root2 = new Group();
         Scene scene = new Scene(root);
+
+        Image icon = new Image("tictactoe.jpeg");
+        stage.getIcons().add(icon);
+        stage.setTitle("Tic Tac Toe Markop");
+        stage.setWidth(500);
+        stage.setHeight(500);
+
+
+
         stage.setScene(scene);
         stage.show();
     }
