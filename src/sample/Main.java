3aed14955bafb65560fee949dfe63b3cd752d9bc
Kostas Markopoulos <kostasmark151@gmail.com>
2022-04-02 19:03:52 +0300

    initial Commit : JavaFX

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import J
 
 public class Main extends Application {
 
@@ -19,14 +20,15 @@ public class Main extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception{
+        //Apla fortwnei to SceneBuilder
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        //Group root2 = new Group();
+        //Group root2 = new Group(); <-- Auto se periptwsh pou den dhmiorgw stage mesa sthn start
         Scene scene = new Scene(root);
 
         Image icon = new Image("tictactoe.jpeg");
         stage.getIcons().add(icon);
-        stage.setTitle("Tic Tac Toe Markop");
-        stage.setWidth(500);
+        stage.setTitle("Tic Tac Toe ");
+        stage.setWidth(600);
         stage.setHeight(500);
 
 
