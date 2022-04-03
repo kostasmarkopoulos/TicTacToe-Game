2566073160e89ba9df7bc81453618445446cdb51
Kostas Markopoulos <kostasmark151@gmail.com>
2022-04-03 23:21:40 +0300

    initial Commit : JavaFX

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,43 +1,83 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
-import java.awt.*;
-import java.awt.event.*;
-import java.util.*;
+import java.util.ResourceBundle;
 
-public class Controller implements ActionListener {
+public class Controller implements Initializable {
 
     Random random = new Random();
-    String textfield = new String();
-    JButton [] buttons = new JButton[9];
     boolean player1_turn;
 
-    Controller(){
-        firstplayer();
-    }
+    @FXML
+    private Text winnerText;
 
+    @FXML
+    private Text firstPickText;
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Button button2;
+
+    @FXML
+    private Button button3;
+
+    @FXML
+    private Button button4;
+
+    @FXML
+    private Button button5;
+
+    @FXML
+    private Button button6;
+
+    @FXML
+    private Button button7;
+
+    @FXML
+    private Button button8;
+
+    @FXML
+    private Button button9;
+
+    Button [] buttons ;
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        buttons = new Button[]{button1, button2, button3, button4, button5, button6, button7, button8, button9};
+
+
 
     }
+
 
     public void firstplayer(){
 
         if (random.nextInt(2)==0){
             player1_turn = true;
-            System.out.println("Its Xs turn");
+            firstPickText.setText("X turn");
         }
         else{
             player1_turn=false;
-            System.out.println("Its Os turn");
+            firstPickText.setText("O turn");
         }
 
+
     }
+
+   // public void setupButton(Button button){
+      //  button.setOnMouseClicked(mouseEvent -> {firstplayer(button);
+          //  button.setDisable(true);});
+    //}
 
     public void checkCombination(){
 
@@ -54,4 +94,6 @@ public class Controller implements ActionListener {
     public void score(){
 
     }
+
+
 }
